e8a7a6354e5ec6fc49d1fe8e896846bd072d6fd0
carlosarturo636 <catamayomojica@gmail.com>
2026-08-19 18:43:49 -0500

    fix: track Meta ViewContent on initialization

--- components/MetaPixel.tsx
+++ components/MetaPixel.tsx
@@ -1,20 +1,10 @@
-"use client";
-
 import Script from "next/script";
-import { getMufasaMetaParameters, trackMetaPixel } from "@/lib/meta-pixel";
 
 const META_PIXEL_ID = "941801464865360";
-let hasTrackedViewContent = false;
 
 export function MetaPixel() {
-  function trackInitialProductView() {
-    if (hasTrackedViewContent) return;
-    hasTrackedViewContent = true;
-    trackMetaPixel("ViewContent", getMufasaMetaParameters());
-  }
-
   return (
-    <Script id="mufasa-meta-pixel" strategy="afterInteractive" onReady={trackInitialProductView}>
+    <Script id="mufasa-meta-pixel" strategy="afterInteractive">
       {`
         !function(f,b,e,v,n,t,s)
         {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
@@ -26,6 +16,12 @@ export function MetaPixel() {
         'https://connect.facebook.net/en_US/fbevents.js');
         fbq('init', '${META_PIXEL_ID}');
         fbq('track', 'PageView');
+        fbq('track', 'ViewContent', {
+          content_name: 'MUFASA',
+          content_type: 'product',
+          value: 120000,
+          currency: 'COP'
+        });
       `}
     </Script>
   );
